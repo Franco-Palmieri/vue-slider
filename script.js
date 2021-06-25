7a7b4087ac1afdfef7677843ce54810f04150214
Ncesecrede <francescopm95@gmail.com>
2021-06-25 14:30:16 +0100

    Vue Js Slider

--- script.js
+++ script.js
@@ -4,13 +4,32 @@ const app = new Vue(
         data: {
             pictures: [
                 "./img/picture-1.jpg",
-                "./img/picture-1.jpg",
-                "./img/picture-1.jpg",
-                "./img/picture-1.jpg"
+                "./img/picture-2.jpg",
+                "./img/picture-3.jpg",
+                "./img/picture-4.jpg"
             ],
+            pictureIndex: 0,
+        },
+        created (){
+            setInterval(() => {
+                this.next();
+            }, 4000)
         },
         methods: {
-            
+            next: function(){
+                if(this.pictureIndex === (this.pictures.length - 1)){
+                    this.pictureIndex = 0;
+                }else{
+                    this.pictureIndex++;
+                }
+            },
+            prev: function(){
+                if(this.pictureIndex === 0){
+                    this.pictureIndex = this.pictures.length - 1;
+                }else{
+                    this.pictureIndex--;
+                }   
+            }
         },
     }
 );
